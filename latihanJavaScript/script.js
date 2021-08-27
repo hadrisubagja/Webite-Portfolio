13330dc7fe318b98f104c9134ed7d7782933cf09
Hadri Subagja <86777948+hadrisubagja@users.noreply.github.com>
2021-08-27 11:33:34 +0700

    Add files via upload
    upload file

---
+++ latihanJavaScript/script.js
@@ -0,0 +1,18 @@
+// latihan pengkondisian (if)
+var jmlAngkot = 10,
+    angkotBeroperasi = 6,
+    noAngkot = 1
+
+for(noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++ ){
+    if (noAngkot <= angkotBeroperasi && noAngkot !== 5) {
+     console.log('Angkot No.' + noAngkot + ' beroperasi dengan baik.')   
+    } else if(noAngkot === 8 || noAngkot === 10 || noAngkot === 5){
+        console.log('Angkot No.' + noAngkot + ' sedang lembur')
+    } else{
+        console.log('Angkot No.' + noAngkot + ' sedang tidak beroperasi')
+    }
+
+    
+}
+
+
